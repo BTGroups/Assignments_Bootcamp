40d52a3fe62c3f5a3835b488751b039af5e21768
Rahul Saravanan <128274814+BlastBringer@users.noreply.github.com>
2025-12-10 19:17:28 +0530

    backlog.md

---
+++ week1_assignment/codingchallenge1.py
@@ -0,0 +1,15 @@
+
+def sumoftwonums(x,y):
+    return x + y 
+
+def averageoftwonums(x,y):
+    return (x + y)/ 2
+
+if __name__ == "__main__":
+    #first and second variables
+    a = 5
+    b = 10 
+    #average and sum of two variables
+    print("Sum of the two variables:",sumoftwonums(a,b))
+    print("Average of the two variables:",averageoftwonums(a,b))
+    
